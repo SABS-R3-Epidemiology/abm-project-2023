b001ca7e46907ebe81a3ffa12d617b0bbf788dbf
Abbie Evans <abbieevans75@gmail.com>
2023-10-24 09:43:10 +0100

    New name for directory

---
+++ abm_model/tests/test_minicell.py
@@ -0,0 +1,41 @@
+import sys
+import unittest
+from unittest import TestCase
+
+import abm_model as abmm
+
+class TestMinicell(TestCase):
+
+    def setUp(self) -> None:
+        self.minicell = abmm.Minicell()
+
+    def test__init__(self):
+        """
+        Test the initialisation function in minicell.py
+        """
+        self.assertEqual(self.minicell.P, self.P)
+        self.assertEqual(self.minicell.D, self.D)
+        self.assertEqual(self.minicell.current_time, 0)
+        self.assertEqual(self.minicell.name, 'test')
+
+    def test_handle(self):
+        """
+        Test the 'handle' function in minicell.py
+        """
+
+
+    def test_update(self):
+        """
+        Test the 'update' function in minicell.py
+        """
+        self.assertEqual(self.microcell.current_time, 0)
+        self.microcell.update(2.0)
+        self.assertEqual(self.microcell.current_time, 2.0)
+
+    def test_write_csv(self):
+        """
+        Test the 'write_csv' function in minicell.py
+        """
+
+if __name__ == '__main__':
+    unittest.main()

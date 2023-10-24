7eeb9392c20a4c13e22c3fd346c89c0bf860a867
Matthew Ghosh <matthew.ghosh88@gmail.com>
2023-10-24 13:40:09 +0100

    Splitting file up into multiple smaller functions

--- abm_model/plot.py
+++ abm_model/plot.py
@@ -3,14 +3,14 @@ import numpy as np
 import os
 
 
-def plot(title: str = ""):
+def plot_data(title: str = ""):
     """
 
     :param title: An optional title for the plot_data file
     :return:
     """
 
-    with open("plot_data_" + title + ".csv", "r") as csv_file:
+    with open("../data/plot_data_" + title + ".csv", "r") as csv_file:
         # Structure of csv file (with no whitespace in between):
         # Time,          Susceptible,     Infected,     Recovered,
         # 1,             90,              10,           0,
@@ -18,65 +18,105 @@ def plot(title: str = ""):
         # ...,           ...,             ...,          ...,
         lines = csv_file.readlines()
         categories = lines[0].split(",")
-        if len(lines) > 1:
 
-            # Here we set up the different lists containing all values
-            data_dict = {category: [] for category in categories}
+        # We will exit this method if there are 0 or 1 lines only in the file as
+        # there is nothing to plot
 
-            for line in lines[1:]:
+        if len(lines) <= 1:
+            return
 
-                # We must check that the length of values_list is 4 and that each value
-                # can be parsed to an int
-                values_list = line.split(",")
+        # Here we set up the different lists containing all values
+        data_dict = {category: [] for category in categories}
 
-                if values_list != 4:
-                    raise ValueError("Each line in the .csv file must have 4 values")
+        for line in lines[1:]:
+            line = line.strip()
 
-                try:
-                    values_list = [int(value) for value in values_list]
+            # We must check that the length of str_values_list is 4 and that each value
+            # can be parsed to an int
+            str_values_list = line.split(",")
 
-                    for value in values_list:
-                        if value < 0:
-                            raise ValueError("All values in the .csv file must be non-negative")
+            values_list = check_validity(str_values_list)
 
-                    # This is checking that the current time step is the first one
-                    if values_list[0] == 1:
-                        # Here, we can infer the total number of individuals and the initial number of infected.
-                        # We will use this to label the plots.
-                        N = sum(values_list[1:])
-                        I_0 = values_list[2]
+            # This is checking that the current time step is the first one
+            if values_list[0] == 0:
+                # Here, we can infer the total number of individuals and the initial number of infected.
+                # We will use this to label the plots.
+                total_people = sum(values_list[1:])
+                initial_infected = values_list[2]
 
-                except ValueError:
-                    raise ValueError("Each value in the .csv file must be an integer")
+            for i, category in enumerate(categories):
 
-                for i, category in enumerate(categories):
+                # This line will add a value to the correct category. E.g. if there is a 90
+                # in the Susceptible column, then here we add 90 to the Susceptible list inside
+                # the data_dict
+                data_dict[category].append(values_list[i])
 
-                    # This line will add a value to the correct category. E.g. if there is a 90
-                    # in the Susceptible column, then here we add 90 to the Susceptible list inside
-                    # the data_dict
-                    data_dict[category].append(values_list[i])
+        # Now we can create the numpy arrays and plots for each category
+        time_array = np.array(data_dict["Time"])
+        for status in categories[1:]:
 
-            # Now we can create the numpy arrays and plots for each category
-            time_array = np.array(data_dict["Time"])
-            for status in categories[1:]:
+            # This will be a numpy array of one of the statuses (Susceptible, Infected or Recovered)
+            status_array = np.array(data_dict[status])
+            plt.plot(time_array, status_array, label=status)
 
-                # This will be a numpy array of one of the statuses (Susceptible, Infected or Recovered)
-                status_array = np.array(data_dict[status])
-                plt.plot(time_array, status_array, label=status)
+        # Here we create the plot legends
+        create_plot_legend(total_people)
 
-            # Here we create the plots
-            plt.legend()
-            plt.ylabel("Number of individuals")
-            plt.xlabel("Time step")
+        # And here we will write to a plots folder
+        plot_path = "../data/plots"
+        create_plot_files(plot_path, total_people, initial_infected)
 
-            plt.title("Agent Based Model for " + str(N) + " individuals in a room")
 
-            # And here we will write to a plots folder
-            plot_folder = "plots"
-            if not os.path.exists(plot_folder):
-                os.makedirs(plot_folder)
+def check_validity(str_values_list: list[str]):
 
-            file_name = "total_" + str(N) + "_initial_" + str(I_0)
+    """This method checks that `str_values_list` is of the correct length and that
+    the internal values are valid. If this is not the case, then errors will be raised.
 
-            destination = plot_folder + "/" + file_name + ".png"
-            plt.savefig(destination)
+    :param str_values_list: A inputted list of strings
+    :return: The list of strings converted into ints (if they are valid)
+    """
+
+    if len(str_values_list) != 4:
+        raise ValueError("Each line in the .csv file must have 4 values")
+
+    try:
+        values_list = [int(value) for value in str_values_list]
+
+        for value in values_list:
+            if value < 0:
+                raise ValueError("All values in the .csv file must be non-negative")
+
+        return values_list
+
+    except ValueError:
+        raise ValueError("Each value in the .csv file must be an integer")
+
+
+def create_plot_files(plot_path: str, total_people: int, initial_infected: int):
+    """This will send the files to the correct location
+
+    :param plot_path: The path pointing to the directory we wish to write into
+    :param total_people: Total number of people in the model
+    :param initial_infected: Initial number of people infected
+    :return:
+    """
+    if not os.path.exists(plot_path):
+        os.makedirs(plot_path)
+
+    file_name = "total_" + str(total_people) + "_initial_" + str(initial_infected)
+
+    destination = plot_path + "/" + file_name + ".png"
+    plt.savefig(destination)
+
+
+def create_plot_legend(total_people: int):
+    """We create the plot legend and title here
+
+    :param total_people: Total number of people in the model
+    :return:
+    """
+    plt.legend()
+    plt.ylabel("Number of individuals")
+    plt.xlabel("Time step")
+
+    plt.title("Agent Based Model for " + str(total_people) + " individuals in a room")

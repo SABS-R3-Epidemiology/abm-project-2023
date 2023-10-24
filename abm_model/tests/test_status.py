7031bedfd79d266c291f3121a449e0860543cb18
Abbie Evans <abbieevans75@gmail.com>
2023-10-24 23:32:22 +0100

    Updated tests

--- abm_model/tests/test_status.py
+++ abm_model/tests/test_status.py
@@ -1,3 +1,4 @@
+import numpy as np
 import unittest
 from unittest import TestCase
 
@@ -10,9 +11,8 @@ class TestSusceptible(TestCase):
 
     def test__repr__(self):
         """
-        
+        Test the '__repr__' function of class Susceptible in status.py
         """
-        self.susceptible = Susceptible()
         self.assertEqual(f"Susceptible", "Susceptible")
 
 
@@ -26,6 +26,15 @@ class TestInfected(TestCase):
         self.assertRaises(ValueError, self.infected.__init__, -1)
         self.infected = Infected(threshold=2)
         self.assertEqual(self.infected.threshold, 2)
+        # Need test for self.expiry_date
+        # Think about how to deal with the random function
+        self.infected = Infected(recovery_period=3, current_time=1)
+        self.assertEqual(self.infected.expiry_date, 2 + np.random.poisson(2))
+        # Check status.py current_time 
+
+    def test__repr__(self):
+        self.assertEqual(f"Susceptible", "Susceptible")
+
 
 
 if __name__ == '__main__':

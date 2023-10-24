7bc230e8d6ae7f31fbf6133bd0f5d00e95b42dfb
Matthew Ghosh <matthew.ghosh88@gmail.com>
2023-10-24 16:49:51 +0100

    Fixed some errors

--- abm_model/command_line.py
+++ abm_model/command_line.py
@@ -28,41 +28,44 @@ except getopt.GetoptError:
     print("Error: incorrect arguments provided. Use '--help' option for help.")
     sys.exit()
 
-names = list(zip(*options))[0]
+if len(options) >= 1:
+    names = list(zip(*options))[0]
 
-for name, value in options:
-    if name in ['-h', '--help']:
-        with open(dirname + "/help.txt", "r") as file:
-            text = file.read()
-        sys.exit()
-    elif name in ['-N', '--population-size']:
-        try:
-            population_size = int(value)
-        except ValueError:
-            print("Error: population size should be an int")
+    for name, value in options:
+        if name in ['-h', '--help']:
+            with open(dirname + "/help.txt", "r") as file:
+                text = file.read()
+                print()
+                print(text)
             sys.exit()
-    elif name in ['-b', '--beta']:
-        try:
-            beta = float(value)
-        except ValueError:
-            print("Error: beta value should be a float or an int")
-            sys.exit()
-    elif name in ['-D', '--recovery-period']:
-        try:
-            recovery_period = float(value)
-        except ValueError:
-            print("Error: recovery period should be a float or an int")
-            sys.exit()
-    elif name in ['-I', '--initial-infected']:
-        try:
-            I_0 = int(value)
-        except ValueError:
-            print("Error: initial number of infected should be an int")
-            sys.exit()
-    elif name in ['-t', '--title']:
-        title = value
-    elif name in ['-p', '--path']:
-        path = value
+        elif name in ['-N', '--population-size']:
+            try:
+                population_size = int(value)
+            except ValueError:
+                print("Error: population size should be an int")
+                sys.exit()
+        elif name in ['-b', '--beta']:
+            try:
+                beta = float(value)
+            except ValueError:
+                print("Error: beta value should be a float or an int")
+                sys.exit()
+        elif name in ['-D', '--recovery-period']:
+            try:
+                recovery_period = float(value)
+            except ValueError:
+                print("Error: recovery period should be a float or an int")
+                sys.exit()
+        elif name in ['-I', '--initial-infected']:
+            try:
+                I_0 = int(value)
+            except ValueError:
+                print("Error: initial number of infected should be an int")
+                sys.exit()
+        elif name in ['-t', '--title']:
+            title = value
+        elif name in ['-p', '--path']:
+            path = value
 
     Minicell(I0=I_0, population_size=population_size, beta=beta, recovery_period=recovery_period,
              name=title, path=path)

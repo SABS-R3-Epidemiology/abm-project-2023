db5fda4a7e3df4260952bd03ec65b436d6a23bfe
Abbie Evans <abbieevans75@gmail.com>
2023-10-26 12:01:35 +0100

    Added extensions

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@ author = "'Matthew Ghosh', 'Abbie Evans', 'Tom Reed', 'Yunli Qi', 'Antonio Mastr
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx.ext.viewcode']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode', 'sphinx.ext.napoleon']
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
